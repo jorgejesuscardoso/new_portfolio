8d6599979fe0e0667039b46f1b294fd5167c7b9d
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2024-04-23 04:00:22 -0300

    refat css

--- frontend/portfolio/src/components/nav/style.tsx
+++ frontend/portfolio/src/components/nav/style.tsx
@@ -12,16 +12,20 @@ export const MainNavbar = styled.nav`
       justify-content: space-around;
       align-items: center;
       width: 100%;
+
         li {
           display: flex;
           justify-content: center;
           align-items: center;
           height: 80%;
           width: 20%;
+          border-bottom: transparent;
+
           &:hover {
             border-bottom: 1px solid var(--border-color-1);
           }
         }
+
         a {
           display: flex;
           align-items: center;
@@ -31,13 +35,17 @@ export const MainNavbar = styled.nav`
           color: var(--text-color-white);
         }
     }
+
     &.claro {
+
       a {
             color: var(--text-color-black);
           }
+
       li:hover {
         border-bottom: 1px solid var(--border-color-2);
       }
+      
       background-color: var(--nav-color-white) !important;
     }
 `;
