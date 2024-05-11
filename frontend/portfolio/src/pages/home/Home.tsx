4eb3ca552ad5a19d7755fbaa3b373f1172f81632
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2024-05-10 21:29:58 -0300

    mudança nas importações

--- frontend/portfolio/src/pages/home/Home.tsx
+++ frontend/portfolio/src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { MainHome } from './style';
 import { useSelector } from 'react-redux';
 import { ChangeThemeRedux } from '../../Types';
-import { PresentationSection } from '../../components/sections/home/Presentation';
+import PresentationSection from '../../components/sections/home/Presentation';
 
 const Home = () => {
   const themeSelector = useSelector((state:ChangeThemeRedux) => state.changeThemeReduce )
